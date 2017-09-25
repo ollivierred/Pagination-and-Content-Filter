fa54e25771d8fa132929856d5fd5a03f09bb91e5
ollivierred <ollivierre.dev@gmail.com>
2017-09-25 09:35:26 -0500

    building search components

--- pagination.js
+++ pagination.js
@@ -1,20 +1,45 @@
-let students = Array.from(document.querySelectorAll('li.student-item'));
-const listLength = students.length;
-var pageList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-var chunk = 10;
-var temp;
+const students = document.querySelectorAll('.student-item');
+const pageHeader = document.querySelector('div.page-header');
+const searchDiv = document.createElement('div');
+searchDiv.className = 'student-search';
 
-for (let j = 0; j < listLength; j++) {
-  students[j].style.display = 'none';
-}
+const searchField = document.createElement('input');
+searchField.type = 'text';
+searchField.placeholder = 'Search for students...';
 
+const searchButton = document.createElement('button');
+searchButton.textContent = 'Search';
 
-function myFunc() {
-  for (let i = 0; i < listLength; i += chunk) {
-    temp = students.slice(i, i + chunk);
-    return temp;
+searchDiv.appendChild(searchField);
+searchDiv.appendChild(searchButton);
+pageHeader.appendChild(searchDiv);
+
+searchButton.addEventListener('click', (e) => {
+  const isClicked = e.target;
+  const lis = document.querySelectorAll('div.student-details');
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].style.backgroundColor = 'red';
   }
-};
+  console.log(lis);
+});
 
-console.log(myFunc());
-console.log( 'list is ' + listLength);
+// let students = Array.from(document.querySelectorAll('li.student-item'));
+// const listLength = students.length;
+// var tempArr = [];
+// var c = 0;
+// var size;
+//
+// for (let j = 0; j < listLength; j++) {
+//   students[j].style.display = 'none';
+// }
+//
+// function myFunc(arr, size) {
+//   for (let i = 0; i < Math.ceil(listLength / size); i++) {
+//     tempArr[i] = arr.slice(c, c + size);
+//     c = c + size;
+//   }
+//   return tempArr;
+// };
+//
+// myFunc(students, 10)
+// console.log( 'list is ' + listLength);
