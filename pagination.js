d583a803f22a942a93c7f204171af316e4393027
ollivierred <ollivierre.dev@gmail.com>
2017-09-27 01:30:46 -0500

    Added early phase functions for pagination

--- pagination.js
+++ pagination.js
@@ -1,53 +1,84 @@
 
+const studentList = Array.from(document.getElementsByTagName('h3'));
+const pageHeaderDiv = document.querySelector('div.page-header');
+const pageDiv = document.querySelector('div.page');
 
-const students = document.getElementsByTagName('h3');
-const pageHeader = document.querySelector('div.page-header');
-const searchDiv = document.createElement('div');
-searchDiv.className = 'student-search';
+const paginationDiv = document.createElement('div');
+const paginationUl = document.createElement('ul');
 
 const form = document.createElement('form');
-const searchField = document.createElement('input');
-searchField.type = 'text';
-searchField.placeholder = 'Search for students...';
+const searchDiv = document.createElement('div');
+const searchInput = document.createElement('input');
 const searchButton = document.createElement('button');
+
+paginationDiv.className = 'pagination';
+searchDiv.className = 'student-search';
+searchInput.type = 'text';
+searchInput.placeholder = 'Search for students...';
 searchButton.textContent = 'Search';
 
 searchDiv.appendChild(form);
-form.appendChild(searchField);
+form.appendChild(searchInput);
 form.appendChild(searchButton);
-pageHeader.appendChild(searchDiv);
+pageHeaderDiv.appendChild(searchDiv);
+
+pageDiv.appendChild(paginationDiv);         //Appends 'div' with the class 'pagination' to page div
+paginationDiv.appendChild(paginationUl);    //Appends a 'ul' to paginationDiv
+     //Appends a 'li' the 'ul'
 
 
 
-//Event listener for search click
-form.addEventListener('submit', (e) => {
+
+form.addEventListener('submit', (e) => {    //Event listener for search click
   e.preventDefault();
-
-  function searchFilter() {
-    // Declare variables
-    //Stores event refernce -> submit
-    const isClicked = e.target;
-    //Stores input value from input field
-    //Converts string to uppercase, handles case sensetivity
-    const filter = searchField.value.toUpperCase();
-
-    //Outputs the students names to the console
-    for (let j = 0; j < students.length; j++) {
-      //Stores text content of the H3 element
-      //Converts string to uppercase, handles case sensetivity
-      let listNames = students[j].textContent;
-      //Parent node of h3 -> li
-      let listLi = students[j].parentNode.parentNode;
-      //Main part of filter***********
-      if (listNames.toUpperCase().indexOf(filter) > -1) {
-        listLi.style.display = ''
-      } else {
-        listLi.style.display = 'none';
-      }
-      // console.log(filter);
-      // console.log(listNames);
-      // console.log(listLi);
-    }
-  };
+  const isClicked = e.target;               //Stores event refernce -> submit
+  searchFilter();
 });
-// console.log(students);
+
+
+function searchFilter() {
+  const filter = searchInput.value.toUpperCase();         //Converts string to uppercase, handles case sensetivity
+                                                          //Stores input value from input field
+  for (let j = 0; j < studentList.length; j++) {          //Outputs the students names to the console
+    let listNames = studentList[j].textContent;           //Stores text content of the H3 element
+    let listLi = studentList[j].parentNode.parentNode;    //Parent node of h3 -> li
+    if (listNames.toUpperCase().indexOf(filter) > -1) {   //Returns index any string entered into input field
+      listLi.style.display = ''
+    } else {
+      listLi.style.display = 'none';
+    }
+    // console.log(filter); // console.log(listNames); // console.log(listLi);
+  }
+};
+
+//Creates pagination link elements and numbers
+function attachPaginationLinks(i) {
+  let li = document.createElement('li');
+  let a = document.createElement('a');
+  a.textContent = i + 1;
+  li.appendChild(a);
+  let link = paginationUl.appendChild(li);
+  return link;
+};
+
+//Splits student list and generates link numbers dynamically
+function pageGenerator(arr, size) {
+  const arrLength = arr.length;
+  let arrGroups = [];
+
+  for (let i = 0; i < arrLength; i += size) {
+    pages = arr.slice(i , i + size);
+    arrGroups.push(pages);
+  }
+  let numOfPages = arrGroups.length;
+
+  for (let i = 0; i < numOfPages; i++) {
+    attachPaginationLinks(i);
+  }
+  return arrGroups;
+};
+
+
+pagination = pageGenerator(studentList, 10);
+console.log(pagination);
+// console.log(attachPaginationLinks());
