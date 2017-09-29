7d128134275cf52e43cdd4c9aefe69e21ff7766a
ollivierred <ollivierre.dev@gmail.com>
2017-09-29 10:54:31 -0500

    first commit

--- pagination.js
+++ pagination.js
@@ -1,83 +1,91 @@
 
-const ul = document.querySelector('ul.student-list');
-const h3 = ul.querySelectorAll('h3');
-const pageHeaderDiv = document.querySelector('div.page-header');
-const pageDiv = document.querySelector('div.page');
+/* Targets and stores: page-header, student-list UL, student-item LI,
+and H3 element in student-item */
+const PAGEHEADERDIV = document.querySelector('.page-header');
+const UL = document.querySelector('.student-list');
+const LI = UL.querySelectorAll('.student-item');
+const H3 = document.querySelectorAll('.student-item > .student-details > h3');
 
-const paginationDiv = document.createElement('div');
-const paginationUl = document.createElement('ul');
+//Create div container of form element
+const DIV = document.createElement('div');
+DIV.className = 'student-search';
+PAGEHEADERDIV.appendChild(DIV);
+//Create form element
+const FORM = document.createElement('form');
+DIV.appendChild(FORM);
+//Create form, input element
+const INPUT = document.createElement('input');
+INPUT.setAttribute("type","text");
+INPUT.setAttribute("placeholder","Search for students...");
+FORM.appendChild(INPUT);
+//Create form button
+const BUTTON = document.createElement('button');
+var btnText = document.createTextNode('Search');
+BUTTON.appendChild(btnText);
+FORM.appendChild(BUTTON);
 
-const form = document.createElement('form');
-const searchDiv = document.createElement('div');
-const searchInput = document.createElement('input');
-const searchButton = document.createElement('button');
-
-paginationDiv.className = 'pagination';
-searchDiv.className = 'student-search';
-searchInput.type = 'text';
-searchInput.placeholder = 'Search for students...';
-searchButton.textContent = 'Search';
-
-searchDiv.appendChild(form);
-form.appendChild(searchInput);
-form.appendChild(searchButton);
-pageHeaderDiv.appendChild(searchDiv);
-
-pageDiv.appendChild(paginationDiv);         //Appends 'div' with the class 'pagination' to page div
-paginationDiv.appendChild(paginationUl);    //Appends a 'ul' to paginationDiv
+// console.log(UL);
+// for (let i = 0; i < H3.length; i++) {
+//   console.log(LI[i]);
+//   console.log(H3[i]);
+//   LI[i].setAttribute("style","display: none");
+// };
 
 /* ------------------- FUNCTIONS HERE -------------------- */
-
-function searchFilter() {
-  const filter = searchInput.value.toUpperCase();         //Converts string to uppercase, handles case sensetivity                                                        //Stores input value from input field
-  for (let i = 0; i < h3.length; i++) {          //Outputs the students names to the console
-    let studentNames = h3[i].textContent;           //Stores text content of the H3 element
-    let li = h3[i].parentNode.parentNode;    //Parent node of h3 -> li
-    if (studentNames.toUpperCase().indexOf(filter) > -1) {   //Returns index of any string entered into input field
-      li.style.display = ''
+function searchFilter (filter) {
+  console.log(filter);
+  for (let i = 0; i < LI.length; i++) {    //Outputs the students names to the console
+    let liNames = H3[i].textContent;  //Stores text content of the H3 element
+    if (liNames.toUpperCase().indexOf(filter) > -1) {   //Returns index of any string entered into input field
+      LI[i].setAttribute("style","display: block");
     } else {
-      li.style.display = 'none';
+      LI[i].setAttribute("style","display: none");
     }
-    console.log(studentNames);
-    console.log(li);
+    // console.log(liNames);
+    // console.log(li);
   }
 };
 
-//Creates pagination link elements and numbers
-function attachPaginationLinks(i) {
-  let li = document.createElement('li');
-  let a = document.createElement('a');
-  a.textContent = i + 1;
-  li.appendChild(a);
-  let link = paginationUl.appendChild(li);
-  return link;
-};
-
-//Splits student list and generates link numbers dynamically
-function getNumberOfPages(arr, sizePerPage) {
-  let length = arr.length;
-  return numOfPages = Math.ceil(length / sizePerPage);
-  // for (let i = 0; i < numOfPages; i++) {
-  //   attachPaginationLinks(i);
-  // }
-};
-
-function loadPages(){
-  let numPerPage = 10;
-  let currentPage = 1;
-  let begin = (currentPage - 1) * numPerPage);
-  let end = begin + numPerPage;
-
-  
-}
-
 /* ------------------- EVENT HANDLERS HERE -------------------- */
-form.addEventListener('submit', (e) => {    //Event listener for search click
+//Event listener for search submit
+FORM.addEventListener('submit', (e) => {
   e.preventDefault();
-  const isClicked = e.target;               //Stores event refernce -> submit
-  searchFilter();
-});
+  let filter = INPUT.value.toUpperCase(); //Captures the input value
+  searchFilter(filter);
+}, false );
 
-paginationUl.addEventListener('click', (e) => {
-  if (e.target.tagName == 'A') {}
-});
+
+
+
+
+// //Creates pagination link elements and numbers
+// function attachPaginationLinks(i) {
+//   let li = document.createElement('li');
+//   let a = document.createElement('a');
+//   a.textContent = i + 1;
+//   li.appendChild(a);
+//   let link = paginationUl.appendChild(li);
+//   return link;
+// };
+
+// //Splits student list and generates link numbers dynamically
+// function getNumberOfPages(arr, sizePerPage) {
+//   let length = arr.length;
+//   return numOfPages = Math.ceil(length / sizePerPage);
+//   // for (let i = 0; i < numOfPages; i++) {
+//   //   attachPaginationLinks(i);
+//   // }
+// };
+
+// function loadPages(){
+//   let numPerPage = 10;
+//   let currentPage = 1;
+//   let begin = (currentPage - 1) * numPerPage);
+//   let end = begin + numPerPage;
+// }
+
+
+
+// paginationUl.addEventListener('click', (e) => {
+//   if (e.target.tagName == 'A') {}
+// });
